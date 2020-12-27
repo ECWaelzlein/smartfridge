90804c16c8db0051aff88de269951e6e795e7f41
Thilo Graffe <thilograffe@gmail.com>
2020-12-27 14:30:27 +0100

    Added model for recipe.

---
+++ src/main/java/de/isemwaf/smartFridge/model/Recipe.java
@@ -0,0 +1,103 @@
+package de.isemwaf.smartFridge.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+public class Recipe extends SuperEntity{
+    private long id;
+    private String name;
+    private String ingredients;
+    private String steps;
+    private float proteins;
+    private float carbs;
+    private float fats;
+    private float calories;
+    private float servings;
+
+    @Id
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(length = 50, nullable = false)
+    @NotBlank
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Column(nullable = false)
+    @NotBlank
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(String ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    @Column(nullable = false)
+    @NotBlank
+    public String getSteps() {
+        return steps;
+    }
+
+    public void setSteps(String steps) {
+        this.steps = steps;
+    }
+
+    @Column(nullable = false)
+    public float getProteins() {
+        return proteins;
+    }
+
+    public void setProteins(float proteins) {
+        this.proteins = proteins;
+    }
+
+    @Column(nullable = false)
+    public float getCarbs() {
+        return carbs;
+    }
+
+    public void setCarbs(float carbs) {
+        this.carbs = carbs;
+    }
+
+    @Column(nullable = false)
+    public float getFats() {
+        return fats;
+    }
+
+    public void setFats(float fats) {
+        this.fats = fats;
+    }
+
+    @Column(nullable = false)
+    public float getCalories() {
+        return calories;
+    }
+
+    public void setCalories(float calories) {
+        this.calories = calories;
+    }
+
+    @Column(nullable = false)
+    public float getServings() {
+        return servings;
+    }
+
+    public void setServings(float servings) {
+        this.servings = servings;
+    }
+}
