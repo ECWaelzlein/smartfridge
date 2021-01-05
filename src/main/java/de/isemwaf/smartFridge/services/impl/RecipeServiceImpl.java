d09c346ee0c77d9ab332caf1609ced5d237ad995
Thorben <t0astbr0d@gmx.de>
2021-01-05 14:04:24 +0100

    Changed Utility (added getJsonAnswer, getRecipeInformationFromJson) Added getRecipeBasedOnIngredients to Service and impl Added UtilityTest to test getRecipeInformationFromJson

--- src/main/java/de/isemwaf/smartFridge/services/impl/RecipeServiceImpl.java
+++ src/main/java/de/isemwaf/smartFridge/services/impl/RecipeServiceImpl.java
@@ -1,17 +1,20 @@
 package de.isemwaf.smartFridge.services.impl;
 
 import de.isemwaf.smartFridge.model.Recipe;
+import de.isemwaf.smartFridge.model.json.IngredientList;
 import de.isemwaf.smartFridge.repositories.RecipeRepository;
 import de.isemwaf.smartFridge.services.RecipeService;
+import de.isemwaf.smartFridge.utility.Utility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
+import java.net.URL;
 
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
     private final RecipeRepository recipeRepository;
+    public static final String sponacularRecipeURL = "https://api.spoonacular.com/recipes/";
+    public static final String sponacularAPIKey = "a1762976c3ac4015aedb6285143cee50";
 
     @Autowired
     public RecipeServiceImpl(RecipeRepository recipeRepository) {
@@ -26,5 +29,31 @@ public class RecipeServiceImpl implements RecipeService {
     @Override
     public Recipe getRecipe(long id) {
         return recipeRepository.findById(id).isPresent() ? recipeRepository.findById(id).get() : null;
+    }
+
+    @Override
+    public Recipe getRecipeBasedOnIngredients(IngredientList ingredientList) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(sponacularRecipeURL);
+        sb.append("findByIngredients?ingredients=");
+        sb.append(ingredientList.getIngredientList().get(0).getIngredient());
+        ingredientList.getIngredientList().remove(0);
+        if (!ingredientList.getIngredientList().isEmpty()){
+            ingredientList.getIngredientList().forEach(s -> sb.append(s.getIngredient()));
+        }
+        sb.append("&number=1&apiKey=");
+        sb.append(sponacularAPIKey);
+        String spectacularQuery = sb.toString();
+        try {
+            String json = Utility.getJsonAnswer(new URL(spectacularQuery));
+            long recipeID = Utility.getFirstRecipeId(json);
+            json = Utility.getJsonAnswer(new URL(sponacularRecipeURL+recipeID+"/information?includeNutrition=true&apiKey="+sponacularAPIKey));
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
--- src/main/java/de/isemwaf/smartFridge/utility/Utility.java
+++ src/main/java/de/isemwaf/smartFridge/utility/Utility.java
@@ -1,7 +1,16 @@
 package de.isemwaf.smartFridge.utility;
 
+import de.isemwaf.smartFridge.model.Recipe;
+import org.springframework.boot.configurationprocessor.json.JSONArray;
 import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Utility {
 
@@ -35,5 +44,61 @@ public class Utility {
         }
 
         return "Not Found";
+    }
+
+    public static String getJsonAnswer(URL apiCall) throws IOException {
+        HttpURLConnection con = (HttpURLConnection) apiCall.openConnection();
+        con.setRequestMethod("GET");
+
+        if (con.getResponseCode() != HttpServletResponse.SC_NOT_FOUND) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String inputLine;
+            StringBuilder content = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+
+            return content.toString();
+        }
+        return null;
+    }
+
+    public static long getFirstRecipeId(String json) throws JSONException {
+
+        JSONObject jsonObject = new JSONArray(json).getJSONObject(0);
+
+        return jsonObject.getLong("id");
+
+    }
+
+    public static Recipe getRecipeInformationFromJson(String jsonString) throws JSONException{
+        JSONObject recipeJSON = new JSONObject(jsonString);
+        JSONObject nutrition = recipeJSON.getJSONObject("nutrition");
+        JSONArray nutrients = nutrition.getJSONArray("nutrients");
+        Recipe recipe = new Recipe();
+        for(int i = 0; i< nutrients.length();i++){
+            JSONObject jsonObject = nutrients.getJSONObject(i);
+            switch (jsonObject.getString("title")) {
+                case "Calories" -> recipe.setCalories((float) jsonObject.getDouble("amount"));
+                case "Carbohydrates" -> recipe.setCarbs((float) jsonObject.getDouble("amount"));
+                case "Protein" -> recipe.setProteins((float) jsonObject.getDouble("amount"));
+                case "Fat" -> recipe.setFats((float) jsonObject.getDouble("amount"));
+            }
+        }
+        recipe.setServings((float) recipeJSON.getDouble("servings"));
+        recipe.setName(recipeJSON.getString("title"));
+        JSONArray extendedIngredients = recipeJSON.getJSONArray("extendedIngredients");
+        JSONArray ingredientsToSave = new JSONArray();
+        for (int i = 0; i < extendedIngredients.length(); i++) {
+            JSONObject jsonObject = extendedIngredients.getJSONObject(i);
+            JSONObject ingredient = new JSONObject();
+            ingredient.put("name", jsonObject.getString("name"));
+            JSONObject measures = jsonObject.getJSONObject("measures").getJSONObject("metric");
+            ingredient.put("amount", measures.getDouble("amount"));
+            ingredient.put("unit", measures.getString("unitShort"));
+        }
+        return recipe;
+
     }
 }
