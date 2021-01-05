27a71759bd39ff270532e7b6fd4d1bd09cafe736
Lissy <l.waelzlein@gmail.com>
2021-01-05 11:51:04 +0100

    Added fridge entity and all services (controller, repository, etc.).

---
+++ src/main/java/de/isemwaf/smartFridge/model/Fridge.java
@@ -0,0 +1,49 @@
+package de.isemwaf.smartFridge.model;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+public class Fridge {
+    private long id;
+    private List<FoodInventory> inventory;
+    private Account account;
+
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "SEQUENCE_FRIDGE_ID"
+    )
+    @SequenceGenerator(
+            name = "SEQUENCE_FRIDGE_ID",
+            sequenceName = "SEQUENCE_FRIDGE_ID",
+            allocationSize = 1
+    )
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @OneToMany
+    @Column
+    public List<FoodInventory> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(List<FoodInventory> inventory) {
+        this.inventory = inventory;
+    }
+
+    @OneToOne
+    @Column
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+}
---
+++ src/main/java/de/isemwaf/smartFridge/services/impl/FridgeServiceImpl.java
@@ -0,0 +1,30 @@
+package de.isemwaf.smartFridge.services.impl;
+
+
+import de.isemwaf.smartFridge.model.Fridge;
+import de.isemwaf.smartFridge.repositories.FridgeRepository;
+import de.isemwaf.smartFridge.services.FridgeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FridgeServiceImpl implements FridgeService {
+
+    private final FridgeRepository fridgeRepository;
+
+    @Autowired
+    public FridgeServiceImpl(FridgeRepository fridgeRepository) {
+        this.fridgeRepository = fridgeRepository;
+    }
+
+
+    @Override
+    public Fridge createFridge(Fridge fridge) {
+        return fridgeRepository.save(fridge);
+    }
+
+    @Override
+    public Fridge getFridge(long id) {
+        return fridgeRepository.findById(id).isPresent() ? fridgeRepository.findById(id).get() : null;
+    }
+}
