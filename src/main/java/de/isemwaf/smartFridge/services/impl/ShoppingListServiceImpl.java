cef1ba66886671e9887b210a95cc1e78b8821d88
Lissy <l.waelzlein@gmail.com>
2021-01-06 17:08:25 +0100

    Added ShoppingList Entity with all services (controller, resository, etc.) Change Meal Entity

---
+++ src/main/java/de/isemwaf/smartFridge/services/impl/ShoppingListServiceImpl.java
@@ -0,0 +1,10 @@
+package de.isemwaf.smartFridge.services.impl;
+
+
+import de.isemwaf.smartFridge.services.ShoppingListService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShoppingListServiceImpl implements ShoppingListService {
+
+}
